b5b9cf22d7b6a20f17ecc26e330a6cfcfa143d2c
icebeam7 <luis.beltran@itcelaya.edu.mx>
2016-08-05 12:15:47 +0200

    Add project files.

---
+++ P03-MisAmigos/P03_MisAmigos.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace P03_MisAmigos.Droid
+{
+	[Activity (Label = "P03_MisAmigos", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new P03_MisAmigos.App ());
+		}
+	}
+}
+
